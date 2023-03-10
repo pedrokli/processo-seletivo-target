b41bbe631ea21c0b1df214489aa69dc05df2a0dd
Pedro Klimaitis <pedro.klimaitis@gmail.com>
2023-03-10 20:48:27 -0300

    first commit

---
+++ src/exercicio3.java
@@ -0,0 +1,20 @@
+
+
+public class exercicio3 {
+	
+	public static void main(String[] args) {
+	
+				int i = 20;
+				System.out.println("fibonacci(" + i + ") = " + fibonacci(i) );
+				
+			
+			
+		
+		
+	}
+	public static int fibonacci(int n) {
+		return n < 2 ? n : (fibonacci(n - 1) + fibonacci (n - 2));
+	}
+
+
+}
